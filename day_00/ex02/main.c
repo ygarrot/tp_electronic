35a813109a7a2c1ace40eedf65bdf5ad34538acd
event <event@e2r2p1.clusters.42paris.fr>
2022-01-10 14:38:09 +0100

    light a led

---
+++ day_00/ex02/main.c
@@ -0,0 +1,13 @@
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main()
+{
+	DDRB |= (1 << DDB1);
+	while (1)
+	{
+		PORTB |= (1 << PORTB3);
+	}
+	return 0;
+}
---
+++ day_00/ex06/main.c
@@ -0,0 +1,16 @@
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main()
+{
+	DDRB |= (1 << DDB1);
+	while (1)
+	{
+		PORTB |= (1 << PORTB1);
+		_delay_ms(1000);
+		PORTB &= ~(1 << PORTB1);
+		_delay_ms(1000);
+	}
+	return 0;
+}
