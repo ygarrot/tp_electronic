f0822ae219b050db14c6f31b85bd2d9257aba7a4
event <event@e2r2p1.clusters.42paris.fr>
2022-01-12 14:53:20 +0100

    clean the doc

--- day_01/ex00/main.c
+++ day_01/ex00/main.c
@@ -1,34 +1,12 @@
 #include <avr/io.h>
-
-/* void main() { */
-/* 	/1* port b 3 en output (led) *1/ */
-/* 	DDRB |= (1 << DDB3); */
-
-/* 	TCCR1B = (1 << CS12) | (1 << CS10); */
-
-/* 	OCR1A = 7812; //16 000 000 /(1024*2) */
-/* 	/1* TCNT1 = 1; *1/ */ 
-/* 	while (1) { */
-/* 		/1* tant que le flag d'overflow n'est pas actif on ne fait rien *1/ */
-/* 		while((TIFR1 & (1 << OCF1A)) == 0); */
-/* 		/1* on inverse l'etat de la led *1/ */
-/* 		PORTB ^= 1 << PORTB3; */
-/* 		/1* TCNT1 = 0; *1/ */ 
-/* 		/1* reset du flag d'overflow *1/ */
-/* 		TIFR1 |= (1<<OCF1A); */
-/* 	} */
-/* } */
-
 void main(){ 
-  /* !! important LED PORT B1 (OC1A) en output*/
+  /* LED PORT B1 (OC1A) en output*/
   DDRB |= (1 << DDB1);
-  /* DDRB |= (1 << DDB3); */
 
   /* Register: TCCR1B */
   /* Name: Timer/Counter 1 Control Register B */
   /* Description: Set the timer mode. */
 
-  // 1 << 2 | 1 << 0 = 101 -> 1024 (Clock/1024)
   TCCR1B = (1 << CS12) | (1 << CS10);
   /* TCCR1B = (1 << CS10); */
   /* Compare Match (CTC) Mode */
@@ -47,7 +25,6 @@ void main(){
   TCCR1A = (1 << COM1A0); //bascule sortie a chaque comparaison
   /* • Bit 5:4 – COM1B1:0: Compare Output Mode for Channel B */
 
-  OCR1A = 7812; //16 000 000 /(1024*2)
-  /* OCR1A = 488; //1 000 000 /(1024*2) */
+  OCR1A = 7812; //16 000 000 / (1024 * 2)
   while(1);
 }
--- day_01/ex01/main.c
+++ day_01/ex01/main.c
@@ -1,14 +1,16 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-
 void main(){ 
+	/* LED PORT B1 (OC1A) en output*/
 	DDRB |= (1 << DDB1);
 
 	/* Register: TCCR1B */
 	/* Name: Timer/Counter 1 Control Register B */
 	/* Description: Set the timer mode. */
 
+	/* | Mode | WGM13 | WGM12 | WGM11 | WGM10 | TimerMode of Operation */
+	/* | 14   | 1     | 1     |   1   |   0    | Fast PWM ICR1 BOTTOM TOP */
 	TCCR1B = (1 << CS12) | (1 << CS10);
 	TCCR1B |= (1 << WGM12);
 	TCCR1B |= (1 << WGM13);
@@ -19,9 +21,8 @@ void main(){
 	TCCR1A = (1 << WGM11); //bascule sortie a chaque comparaison
 	TCCR1A |= (1 << COM1A1);
 
-	/* • Bit 5:4 – COM1B1:0: Compare Output Mode for Channel B */
 	ICR1 = 7812; // F_CPU / 1024 / 2
-	OCR1A = 781;
+	OCR1A = ICR1 / 10;
 	while(1);
 
 }
