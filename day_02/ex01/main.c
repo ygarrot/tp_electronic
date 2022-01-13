c0e83d8f41dd12a21bdc4391674e13a0fa299fce
event <event@e2r2p1.clusters.42paris.fr>
2022-01-13 15:26:29 +0100

    ex01 day 02

--- day_02/ex01/main.c
+++ day_02/ex01/main.c
@@ -1,55 +1,57 @@
 #include <avr/io.h>
 #include <avr/delay.h>
+#include <avr/interrupt.h>
 
-/* static int rapport = 0; */
+#define N_STEP 250
+#define PRESCALE_256 (1 << CS02) | (0 << CS01) | (0 << CS00)
+#define PRESCALE_64 (0 << CS02) | (1 << CS01) | (1 << CS00)
 
-void TIMER0_COMPA_vect (void) __attribute__ ((signal,__INTR_ATTRS));
+void TIMER0_COMPA_vect (void) __attribute__ ((signal));
 
 void TIMER0_COMPA_vect (void) {
-	/* if (OCR0A % 40) */
-		PORTB ^= (1 << PORTB3);
-	/* OCR0A++; */
-	/* if (rapport < 100) */
-	/* 	rapport--; */
-	/* else */
-	/* 	rapport++; */
+	static uint8_t inc = 0;
+	static uint8_t ratio = 0;
+
+	if (inc)
+		ratio++;
+	else
+		ratio--;
+	if (!inc && ratio == 0
+		|| inc && ratio == N_STEP)
+		inc = !inc;
+	OCR1A = ratio;
 }
 
 void duty_cycle() {
-	DDRB |= (1 << DDB1);
-	TCCR1B = (1 << CS12) | (1 << CS10);
+	TCCR1B = PRESCALE_64;
 	TCCR1B |= (1 << WGM12);
 	TCCR1B |= (1 << WGM13);
 
 	TCCR1A = (1 << WGM11);
 	TCCR1A |= (1 << COM1A1);
-	ICR1 = 15624; // F_CPU / 1024 / 2
+	ICR1 = N_STEP;
+	OCR1A = 0;
 }
 
-void init () {
-	OCR0A= 1000;
-	DDRB |= (1 << DDB3);
-
-	TCNT0 = 0; //initialize counter value to 0
-	TCCR0A = (1 << WGM01); //turn on CTC mode
-	TCCR0B = (0 << CS02) | (0 << CS01) | (1 << CS00);
-	
+void init() {
+	// clk / 256 bits
+	TCCR0B = PRESCALE_256;
+	// turn on CTC mode
+	TCCR0A = (1 << WGM01);
+	// trigger on release
 	TIMSK0 = (1 << OCIE0A);
 
-	/* /1* Clear overflow flag *1/ */
-	TIFR0  = 1<<TOV1;
-
-	/* enable global interupt */
+	//(16*10^6) / (1000*64) - 1
+	OCR0A = N_STEP;
 	SREG = (1 << 7);
 }
 
 int main() {
-	/* duty_cycle(); */
+	DDRB = (1 << DDB3);
+	DDRB |= (1 << DDB1);
+
+	duty_cycle();
 	init();
-	while (1) {
-		/* OCR0A++; */
-		/* OCR1A = ICR1 / rapport; */
-		/* OCR1A = ICR1 * rapport / 100; */
-	}
-	return 1;
+
+	while (1);
 }
