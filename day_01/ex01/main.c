ae8384827feb10ebb5d6b31521902c3b36ab8a87
ygarrot <garrot.yoan1@gmail.com>
2022-01-14 14:59:30 +0100

    comment ex01 day 01

--- day_01/ex01/main.c
+++ day_01/ex01/main.c
@@ -1,28 +1,29 @@
-#include <avr/io.h>
-#include <util/delay.h>
+#include "tools.h"
 
 void main(){ 
-	/* LED PORT B1 (OC1A) en output*/
-	DDRB |= (1 << DDB1);
+	B_OUTPUT(1);
 
-	/* Register: TCCR1B */
-	/* Name: Timer/Counter 1 Control Register B */
-	/* Description: Set the timer mode. */
+	/* +------+------+------+---------------------------+ */
+	/* | CS12 | CS11 | CS10 | Description               | */
+	/* |   1  |   0  |   1  | clk/1024 (From prescaler) | */
+	/* +------+------+------+---------------------------+ */
+	TCCR1B = (1 << CS12) | (0 << CS11) | (1 << CS10);
 
-	/* | Mode | WGM13 | WGM12 | WGM11 | WGM10 | TimerMode of Operation */
-	/* | 14   | 1     | 1     |   1   |   0    | Fast PWM ICR1 BOTTOM TOP */
-	TCCR1B = (1 << CS12) | (1 << CS10);
+	/* +------+-------+-------+-------+-------+---------------+--------+-----------+-----------+ */
+	/* | Mode | WGM13 | WGM12 | WGM11 | WGM10 | Timer/Counter |  Top   | Update of | TOV1 Flag | */
+	/* |      |       |       |       |       |  mode of op   |        | OCR1x at  |  Set on   | */
+	/* +------+-------+-------+-------+-------+---------------+--------+-----------+-----------+ */
+	/* |  14  |   1   |   1   |   1   |   0   |    Fast PWM   |  ICR1  |  BOTTOM   |    TOP    | */
+	/* +------+-------+-------+-------+-------+---------------+--------+-----------+-----------+ */
 	TCCR1B |= (1 << WGM12);
 	TCCR1B |= (1 << WGM13);
+	TCCR1A = (1 << WGM11);
 
-	/* Register: TCCR1A */
-	/* Name: Timer/Counter 1 Control Register A */
-	/* Description: Set the timer mode. */
-	TCCR1A = (1 << WGM11); //bascule sortie a chaque comparaison
+	/* |      1  |      0  | Clear OC1A/OC1B on Compare Match (Set output to low level). | */
 	TCCR1A |= (1 << COM1A1);
 
-	ICR1 = 7812; // F_CPU / 1024 / 2
+	/*            // F_CPU / (PRESCALE * 2) */
+	OCR1A = 7812; // 16 000 000 / (1024 * 2)
 	OCR1A = ICR1 / 10;
 	while(1);
-
 }
