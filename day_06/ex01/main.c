644a4022c87d62cca02af4aacc2caa0286799aac
ygarrot <garrot.yoan1@gmail.com>
2022-01-21 10:03:21 +0100

    ex02 - temperature

--- day_06/ex01/main.c
+++ day_06/ex01/main.c
@@ -1,69 +1,49 @@
 #include "tools.h"
-#include <util/delay.h>
 
-MY_ISR(TIMER0_COMPA_vect) {
-	PORTD = ~RED;
+#include <avr/interrupt.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TEMP (int8_t)(((float)ADC - 324.31 ) / 1.22)
+
+ISR(ADC_vect) {
+	char buffer[20];
+	ADCSRA |= (1 << ADSC) | (1 << ADIE);
+	itoa(TEMP, buffer, 10);
+	uart_printstr(buffer);
+	uart_printstr("\r\n");
 }
 
-MY_ISR(TIMER0_COMPB_vect) {
-	PORTD = ~GREEN;
+/* ADIF = ADC Interrupt Flag */
+/* ADIE: ADC Interrupt Enable */
+/* ADEN: ADC Enable */
+/* Selecting the ADC8 channel by writing the MUX3...0 bits in ADMUX register to "1000" enables */
+/* the temperature sensor. The internal 1.1V voltage reference must also be selected for the ADC voltage */
+/* reference source in the temperature sensor measurement. W */
+void ADC_init()
+{
+	ADMUX = (1 << REFS1) |(1 << REFS0) | (1 << MUX3);
+
+	ADCSRA = (1 << ADEN); //enable ADE
+	ADCSRA |= (1 << ADIE); //interrupt
+	ADCSRA |= (1 << ADIF); //interupt
+	ADCSRA |= (1 << ADSC); //A2D CONVERTION
+
+	ADCSRA |= (1 << ADPS2); //prescalers
+	ADCSRA |= (1 << ADPS1); //prescalers
+	ADCSRA |= (1 << ADPS0); //prescalers
+
 }
-
-MY_ISR(TIMER1_COMPA_vect) {
-	PORTD = ~BLUE;
-}
-
-typedef struct rgb {
-	uint8_t r;
-	uint8_t g;
-	uint8_t b;
-} t_rgb;
-
-#define inverse_ratio(n) (int8_t)(100.0 - (n / 255.0 * 100.0))
-#define calc_ratio(n) freq * inverse_ratio(n) / 100;
-
-#define set_red(n) OCR0A = calc_ratio(n);
-#define set_green(n) OCR0B = calc_ratio(n);
-#define set_blue(n) OCR1A = calc_ratio(n);
 
 int main() {
-	int8_t i = 0;
-	const t_rgb colors[3] = {
-		{ .r = 0xf2, .g = 0x18, .b = 0x4f },
-		{ .r = 0x19, .g = 0xe0, .b = 0xa1 },
-		{ .r = 0x4b, .g = 0x00, .b = 0x82 },
-	};
-	TCCR1B = (1 << CS12) | (0 << CS11) | (1 << CS10);
-	TCCR0B = (1 << CS12) | (0 << CS11) | (1 << CS10);
+	DDRD = PORT_OUTPUT(D, 6) | PORT_OUTPUT(D, 5) | PORT_OUTPUT(D, 3);
 
-	TCCR0B |= (1 << WGM02);
-	TCCR0A |= (1 << WGM00);
-
-	TCCR1B |= (1 << WGM12);
-	TCCR1A |= (1 << WGM12);
-
-	GLOBAL_INTERRUPT;
-
-	// Enable CTC interrupt
-	TIMSK1 |= (1 << OCIE1A);
-	TIMSK0 |= (1 << OCIE0A);
-	TIMSK0 |= (1 << OCIE0B);
-	D2_INPUT;
-	DDRD |= PORT_OUTPUT(D, 6) | PORT_OUTPUT(D, 5) | PORT_OUTPUT(D, 3);
-
+	uart_init(8);
+	ADC_init();
+	sei();
+	/* GLOBAL_INTERRUPT; */
 	PORTD = 0xff;
-	while(1) {
-		int8_t freq = 50;
-		if (!(PIND & (1 << PIND2))) {
-			while (!(PIND & (1 << PIND2)));
-			i = (i + 1) % 3;
-			const t_rgb test = colors[i];
-			set_red(test.r);
-			set_green(test.g);
-			set_blue(test.b);
-			_delay_ms(1000);
-		}
-
-	}
+	while(1);
 }
 
